4e76366d91b99ca51ade956c53e2fb0c78ef8b27
DanHutsul <dan.hutsul@gmail.com>
2020-03-31 20:43:52 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,14 +2,22 @@ import praw
 import config
 import time
 import os
-
+# !WARNING!
+# This code is not meant to work without inputting parameters
+# I`m not putting them in because they hold confidential info (e.g. password)
 def bot_login():
 	print "Logging in..."
-	r = praw.Reddit(username = config.username,
-				password = config.password,
-				client_id = config.client_id,
-				client_secret = config.client_secret,
-				user_agent = "The Reddit Commenter v1.0")
+	username = "RedditUsername"
+	password = "password"
+	client_id = "idGoesHere"
+	client_secret = "secretGoesHere"
+	r = praw.Reddit(username = username,
+				password = password,
+				client_id = client_id,
+				client_secret = client_secret,
+				user_agent = "Some item v1.0")
 	print "Logged in!"
 
 	return r
+if __name__ == "__main__":
+	bot_login()

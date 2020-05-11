8c242d31e8aec6db26e46b2387b58b499b819665
DanHutsul <dan.hutsul@gmail.com>
2020-05-11 15:12:32 +0300

    ADT

--- words_dict.py
+++ words_dict.py
@@ -1,42 +1,93 @@
 import praw
+import re
+
 
 class words_dict:
 
     def __init__(self, submission):
+        """
+        Initializes the ADT
+
+        submission : submission() class from praw
+        """
         self.submission = submission
+
+        # Dictionary key: Word
+        # Dectionary value: amount of this word in comments
         self.negative = dict()
         self.positive = dict()
-        self.negative_words = open("negative-words.txt", "r").read()
-        self.positive_words = open("positive-words.txt", "r").read()
+
+        # Two files are required
+        with open("negative-words.txt", "r") as f:
+            self.negative_words = f.read()
+        with open("positive-words.txt", "r") as f:
+            self.positive_words = f.read()
 
     def get(self):
+        """
+        Returns two dictionaries
+        First with Positive words
+        Second with Negative words
+
+        return : dict(), dict()
+        """
+        # Get all words
         words = self._simplify()
+
         for word in words:
             if self._negative(word):
-                if not word in self.negative:
+                if word not in self.negative:
                     self.negative[word] = 1
                 else:
                     self.negative[word] += 1
+
             elif self._positive(word):
-                if not word in self.positive:
+                if word not in self.positive:
                     self.positive[word] = 1
                 else:
                     self.positive[word] += 1
+
         return self.positive, self.negative
 
     def _simplify(self):
-        self.submission.comments.replace_more(limit=None)
-        words = []
-        for word in self.submission.comments.list():
-            words.append(word)
-        return words
+        """
+        Reads all comments from a submission
+        Returns a list with all words
+
+        return : list()
+        """
+        submissionList = []
+        temp = self.submission
+        temp.comments.replace_more(limit=None)
+        for comment in temp.comments.list():
+            submissionList.append(comment.body)
+        words_list = []
+        for comment in submissionList:
+            res = comment.split()
+            for i in res:
+                words_list.append(i)
+        return words_list
 
     def _negative(self, word):
-        if word in self.negative_words:
+        """
+        Determines if word is negative
+
+        word : str()
+        return : bool()
+        """
+        if re.search(r"(?:-^|(?<=\s))" + word +
+                     r"(?=\s|$-)", self.negative_words):
             return True
         return False
 
     def _positive(self, word):
-        if word in self.positive_words:
+        """
+        Determines if word is positive
+
+        word : str()
+        return : bool()
+        """
+        if re.search(r"(?:-^|(?<=\s))" + word +
+                     r"(?=\s|$-)", self.positive_words):
             return True
         return False
